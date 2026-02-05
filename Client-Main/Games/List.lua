5ce5dbc071f98654a92a844aba052250953e8514
Skibidi50-lol <realhatroi@gmail.com>
2026-02-05 22:20:34 +0700

    Update List.lua

--- Client-Main/Games/List.lua
+++ Client-Main/Games/List.lua
@@ -1,7 +1,7 @@
 local Games = {
   --Voxels
-  [102019850130872] = nil, -- Voxelized
-  [120431902078318] = nil  -- Voxelized
+  [102019850130872] = "https://raw.githubusercontent.com/Skibidi50-lol/BloomWare-Client/refs/heads/main/Client-Main/Games/Client-Scripts/Voxelized.lua", -- Voxelized
+  [120431902078318] = "https://raw.githubusercontent.com/Skibidi50-lol/BloomWare-Client/refs/heads/main/Client-Main/Games/Client-Scripts/Voxelized.lua"  -- Voxelized
 }
 
 return Games
